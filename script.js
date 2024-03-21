2d14a39b03350aa880cc553ed483f4a6add229da
B-Ascii <bedir.asici@outlook.com>
2024-03-21 16:54:10 +1300

    printing

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.getElementById('loadIdeasButton').addEventListener('click', function() {
-    fetch('test.json')
+    fetch('ideas.json')
       .then(response => response.json())
       .then(data => {
         console.log('JSON data:', data);
         // Display the JSON data in the output div
-        const names = Object.values(data.name).join('\n');
+        const items = Object.values(data[2].IDEA).join('\n');
         // Display the names in the textarea
         const jsonTextArea = document.getElementById('ideaList');
-        jsonTextArea.value = names;
+        jsonTextArea.value = items;
         // const jsonTextArea = document.getElementById('ideaList');
         // jsonTextArea.value = JSON.stringify(data.name, null, 2);
       })
