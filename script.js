bb0f087e0d313ebf49fd0a10d8a267eb607a7c53
B-Ascii <bedir.asici@outlook.com>
2024-03-21 17:16:27 +1300

    new array

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('loadIdeasButton').addEventListener('click', function() 
       .then(data => {
         //console.log('JSON data:', data);
 
-        var ideas = data.map(entry => entry.IDEA).join(" ");
+        var ideas = data.map(entry => entry.IDEA).join("  ");
         // Display the ideas in the textarea
         const jsonTextArea = document.getElementById('ideaList');
 
@@ -12,7 +12,7 @@ document.getElementById('loadIdeasButton').addEventListener('click', function() 
 
         // const jsonTextArea = document.getElementById('ideaList');
         // jsonTextArea.value = JSON.stringify(data.name, null, 2);
-          const ideasArray= ideas.split(" ");
+          const ideasArray= ideas.split("  ");
           var arrayCounter = 0;
           while (arrayCounter<ideasArray.length){
             jsonTextArea.value += ideasArray[arrayCounter] +"\n";
